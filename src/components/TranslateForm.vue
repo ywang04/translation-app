6a4370fc0df9fe9f48690fcbde679aa3943e114c
Yang Wang <Yang.Wang04@gmail.com>
2018-02-11 11:43:44 +1100

    Create TranslateForm component

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,35 @@
+<template>
+ <div id="translateForm">
+  <form v-on:submit="formSubmit">
+   <input type="text" placeholder="Enter text" v-model="textToTranslate">
+   <select>
+    <option value="en">English</option>
+   </select>
+   <!-- <input type="submit" value="Translate"> -->
+   <button type="submit">Translate</button>
+  </form>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data: function () {
+    return {
+      textToTranslate: '',
+
+    }
+  },
+  methods: {
+    formSubmit: function (e) {
+      // alert(this.textToTranslate)
+      this.$emit('formSubmit', this.textToTranslate)
+      e.preventDefault()
+    }
+  }
+}
+
+</script>
+<style scoped>
+
+</style>
