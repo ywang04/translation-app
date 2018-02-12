87ec2aae39da2948e84077a7561493305178dcb1
Yang Wang <Yang.Wang04@gmail.com>
2018-02-12 15:58:36 +1100

    Add some languages in dropdown list

--- src/components/TranslateForm.vue
+++ src/components/TranslateForm.vue
@@ -2,8 +2,13 @@
  <div id="translateForm">
   <form v-on:submit="formSubmit">
    <input type="text" placeholder="Enter text" v-model="textToTranslate">
-   <select>
-    <option value="en">English</option>
+   <select v-model="language">
+    <option value="zh">Chinese</option>
+    <option value="fr">French</option>
+    <option value="de">German</option>
+    <option value="ja">Japanese</option>
+    <option value="ko">Korean</option>
+    <option value="es">Spanish</option>
    </select>
    <!-- <input type="submit" value="Translate"> -->
    <button type="submit">Translate</button>
@@ -17,18 +22,16 @@ export default {
   data: function () {
     return {
       textToTranslate: '',
-
+      language: ''
     }
   },
   methods: {
     formSubmit: function (e) {
-      // alert(this.textToTranslate)
-      this.$emit('formSubmit', this.textToTranslate)
+      this.$emit('formSubmit', this.textToTranslate, this.language)
       e.preventDefault()
     }
   }
 }
-
 </script>
 <style scoped>
 
