882fae337fcaf066adf60c4f6adc18caf93b8970
Yang Wang <Yang.Wang04@gmail.com>
2018-02-12 12:12:45 +1100

    Register formSubmit event and pass data from TranslateForm.vue to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  <div id="app">
   <h1>Online Translation</h1>
   <h5>Simple / Easy to Use / Convenient</h5>
-  <translateForm></translateForm>
+  <translateForm v-on:formSubmit="translateText($event)"></translateForm>
   <translateOutput></translateOutput>
  </div>
 </template>
@@ -20,10 +20,26 @@ export default {
     TranslateForm,
     TranslateOutput
 
+  },
+  methods: {
+    translateText: function (text) {
+      let url = 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180211T012317Z.e40553e0346713a2.e8d100804e1839c82cc4f0a882aed8f01799f9fc&lang=en&text=' + text
+      this.$http.get(url).then((response) => {
+        console.log(response)
+        console.log(response.body.text[0])
+      })
+    }
   }
 }
 </script>
 
 <style>
-
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
